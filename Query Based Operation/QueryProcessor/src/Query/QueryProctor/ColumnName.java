744cc28c09795fbca9df777f22d0d259ea6f6aea
sonofvanu <yashyajnesh@gmail.com>
2017-07-15 11:05:06 +0530

    after little debugging

--- Query Based Operation/QueryProcessor/src/Query/QueryProctor/ColumnName.java
+++
@@ -1,9 +0,0 @@
-package Query.QueryProctor;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class ColumnName {
-
-	public Map<String, Integer> colnames = new HashMap<>();
-}
--- Query Based Operation/QueryProcessor/src/Query/TestCase/QueryProcessingTester.java
+++ Query Based Operation/QueryProcessor/src/Query/TestCase/QueryProcessingTester.java
@@ -20,15 +20,14 @@ public class QueryProcessingTester {
 	QueryParser queryparser = new QueryParser();
 	FileHandler filehandler = new FileHandler();
 
-	@Test(expected=NullPointerException.class)
+	@Test(expected = NullPointerException.class)
 	public void queryInsertionTest() {
 		System.out.println("Test case 1 a started");
-		
-		for(String string:queryparser.inputQuerryArray())
-		{
+
+		for (String string : queryparser.inputQuerryArray()) {
 			System.out.println(string);
 		}
-		assertNotNull(queryparser.inputQuerryArray().length);
+		assertNull(queryparser.inputQuerryArray().length);
 
 	}
 
@@ -36,28 +35,29 @@ public class QueryProcessingTester {
 	public void queryEligibilityChecker() {
 		System.out.println("Test case 2 a started");
 		queryparser.eligibleQuery("select * from employee.csv");
-		
-		System.out.println(queryparser.eligibleQuery("select * from employee.csv"));
 		assertTrue("im true", queryparser.eligibleQuery("select * from employee.csv"));
+		System.out.println(queryparser.eligibleQuery("select * from employee.csv"));
+
 		System.out.println("Test case 2 a completed");
 
 	}
-//
+
+	//
 	@Test(expected = Exception.class)
 	public void queryEligibilityCheckerNegative() {
 		System.out.println("Test case 2 b started");
 		queryparser.eligibleQuery("select from employee.csv");
 		assertFalse("im false", queryparser.eligibleQuery("select from employee.csv"));
+		System.out.println(queryparser.eligibleQuery("select from employee.csv"));
 		System.out.println("Test case 2 b completed");
 
 	}
-//select * from employee.csv
-
+	// select * from employee.csv
 
 	@Test(expected = Exception.class)
 	public void relationalFieldProcessing() {
 		System.out.println("Test case 4 a started");
-		
+
 		System.out.println(queryparser.relationalExpressionProcessing("empid<12"));
 		assertNull("im not null", queryparser.relationalExpressionProcessing("empid<12"));
 		System.out.println("Test case 4 a Completed");
@@ -66,6 +66,7 @@ public class QueryProcessingTester {
 	@Test(expected = Exception.class)
 	public void relationalFieldProcessingNegative() {
 		System.out.println("Test case 4 b started");
+		System.out.println(queryparser.relationalExpressionProcessing("empid&12"));
 		assertNotNull("im null", queryparser.relationalExpressionProcessing("empid&12"));
 		System.out.println("Test case 4 b Completed");
 
@@ -74,7 +75,7 @@ public class QueryProcessingTester {
 	@Test(expected = Exception.class)
 	public void fieldsSeparator() {
 		System.out.println("Test case 5 a started");
-		
+
 		System.out.println("Column Name: " + queryparser.fieldsSeparator(query2).column);
 		System.out.println("Group by Col Name: " + queryparser.fieldsSeparator(query2).groupbycol
 				+ "Order by Col Name: " + queryparser.fieldsSeparator(query2).orderbycol);
@@ -91,29 +92,32 @@ public class QueryProcessingTester {
 		System.out.println("Test case 5 b completed");
 
 	}
-//
+
+	//
 	@Test(expected = Exception.class)
 	public void headerMap() {
 		System.out.println("Test case 6 a started");
-		
+
 		System.out.println(filehandler.checkingHeaderInColumn(query2).toString());
 		assertNotNull("im not null", filehandler.checkingHeaderInColumn(query2));
 		System.out.println("Test case 6 a completed");
 	}
-//
+
+	//
 	@Test(expected = Exception.class)
 	public void headerMapNegative() {
 		System.out.println("Test case 6 b started");
-		
+
 		System.out.println(filehandler.checkingHeaderInColumn(query3).toString());
 		assertNull("im null", filehandler.checkingHeaderInColumn(query3));
 		System.out.println("Test case 6 b completed");
 	}
-//
+
+	//
 	@Test(expected = Exception.class)
 	public void rowDataMap() {
 		System.out.println("Test case 7 a started");
-		
+
 		System.out.println(filehandler.gettingRowDataMap().toString());
 		assertNotNull("im not null", filehandler.gettingRowDataMap());
 		System.out.println("Test case 7 a completed");
@@ -122,16 +126,17 @@ public class QueryProcessingTester {
 	@Test(expected = Exception.class)
 	public void rowDataMapNegative() {
 		System.out.println("Test case 7 b started");
-		
+
 		System.out.println(filehandler.gettingRowDataMap().toString());
 		assertNull("im null", filehandler.gettingRowDataMap());
 		System.out.println("Test case 7 b completed");
 	}
-//
+
+	//
 	@Test(expected = Exception.class)
 	public void columnDataProcessor() {
 		System.out.println("Test case 8 a started");
-		
+
 		System.out.println(filehandler.ColumnDataProcessor(filehandler.columnSeparator(),
 				filehandler.gettingRowDataMap(), filehandler.gettingHeaderMap()).toString());
 		assertNotNull("im not null", filehandler.ColumnDataProcessor(filehandler.columnSeparator(),
@@ -150,17 +155,22 @@ public class QueryProcessingTester {
 		System.out.println("Test case 8 b completed");
 	}
 
-	
-	
 	@Test
 	public void fieldsProcessor() {
 		System.out.println("Test case 3 a started");
-		Map<String,Integer> expectedMap = new HashMap<>();
-		expectedMap.put("empid",0);
-		expectedMap.put("empname", 1);
 		System.out.println(queryparser.fieldsProcessing("empid,empname").columnames.colnames.toString());
 		assertNotNull(queryparser.fieldsProcessing("empid,empname").columnames.colnames.get("empid"));
-		
 		System.out.println("Test case 3 a completed");
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void fetchingrowdata() throws Exception {
+		System.out.println("Test 9 a started");
+		System.out.println("The final data");
+		for (String string : filehandler.fetchingRowData(queryparser.filepath)) {
+			System.out.println(string);
+		}
+		assertNotNull(filehandler.fetchingRowData(queryparser.filepath));
+		System.out.println("Test 9 a completed");
 	}
 }
