cc79aff2088da388ddd5ce3ddc3019500e15076f
sonofvanu <yashyajnesh@gmail.com>
2017-07-30 09:56:48 +0530

    after adding the cutted test cases

--- DataMungingVersionFinal/test/com/niit/testcase/QueryTestCase.java
+++ DataMungingVersionFinal/test/com/niit/testcase/QueryTestCase.java
@@ -18,12 +18,69 @@ public class QueryTestCase {
 	}
 
 	@Test
-	public void checkingTest() {
+	public void simpleTest() {
 		Map<Integer, String> resultantSet = processController.queryFunctionsCaller("select * from employee.csv");
 		assertNotNull(resultantSet);
-		show("multipleTest", resultantSet);
+		show("simpleTest", resultantSet);
+	}
+	
+	@Test
+	public void columnTest() {
+		Map<Integer, String> resultantSet = processController.queryFunctionsCaller("select empid,empsalary from employee.csv");
+		assertNotNull(resultantSet);
+		show("columnTest", resultantSet);
 	}
 
+	@Test
+	public void whereSingleGreater() {
+		Map<Integer, String> resultantSet = processController.queryFunctionsCaller("select * from employee.csv where empid>3");
+		assertNotNull(resultantSet);
+		show("whereSingleGreater", resultantSet);
+	}
+	
+	
+	@Test
+	public void whereSingleLesser() {
+		Map<Integer, String> resultantSet = processController.queryFunctionsCaller("select * from employee.csv where empsalary<45000");
+		assertNotNull(resultantSet);
+		show("whereSingleLesser", resultantSet);
+	}
+	
+	@Test
+	public void whereSingleEqual() {
+		Map<Integer, String> resultantSet = processController.queryFunctionsCaller("select * from employee.csv where empname=Anand");
+		assertNotNull(resultantSet);
+		show("whereSingleEqual", resultantSet);
+	}
+	
+	@Test
+	public void whereSingleNotEqual() {
+		Map<Integer, String> resultantSet = processController.queryFunctionsCaller("select * from employee.csv where empsalary!=60000");
+		assertNotNull(resultantSet);
+		show("whereSingleNotEqual", resultantSet);
+	}
+	
+	@Test
+	public void whereSingleLessEqual() {
+		Map<Integer, String> resultantSet = processController.queryFunctionsCaller("select * from employee.csv where empsalary>=60000");
+		assertNotNull(resultantSet);
+		show("whereSingleLessEqual", resultantSet);
+	}
+	
+	@Test
+	public void whereSingleGreatEqual() {
+		Map<Integer, String> resultantSet = processController.queryFunctionsCaller("select * from employee.csv where empid<=5");
+		assertNotNull(resultantSet);
+		show("whereSingleGreatEqual", resultantSet);
+	}
+	
+	@Test
+	public void whereMultiple() {
+		Map<Integer, String> resultantSet = processController.queryFunctionsCaller("select * from employee.csv where empid<3 and empsalary>50000 or empdept=delivery");
+		assertNotNull(resultantSet);
+		show("whereMultiple", resultantSet);
+	}
+	
 	public void show(String msg, Map<Integer, String> resultSet) {
 		System.out.println("================================================");
 		System.out.println(msg);
