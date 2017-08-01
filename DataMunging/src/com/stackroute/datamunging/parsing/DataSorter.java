1ccd1038666a22f68b4b1daf5cb4bf4a6ee7f4d0
sonofvanu <yashyajnesh@gmail.com>
2017-08-01 15:49:46 +0530

    some changes in data sorter

---
+++ DataMunging/src/com/stackroute/datamunging/parsing/DataSorter.java
@@ -0,0 +1,25 @@
+package com.stackroute.datamunging.parsing;
+
+import java.util.Comparator;
+
+import com.stackroute.datamunging.model.RowDataHolder;
+
+public class DataSorter implements Comparator<RowDataHolder> {
+	private int sortingIndex;
+
+	public int getSortingIndex() {
+		return sortingIndex;
+	}
+
+	public void setSortingIndex(int sortingIndex) 
+	{
+		this.sortingIndex = sortingIndex;
+	}
+
+	@Override
+	public int compare(RowDataHolder arg0, RowDataHolder arg1) 
+	{
+		return arg0.get(sortingIndex).compareTo(arg1.get(sortingIndex));
+	}
+
+}
